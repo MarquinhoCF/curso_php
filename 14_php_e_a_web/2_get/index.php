1c676848e4afc6d7b1c6b8603b8ed84b8db15736
MarquinhoCF <marcos13@gmail.com>
2024-03-04 15:54:47 -0300

    Conclusão da seção: PHP e a web

---
+++ 14_php_e_a_web/2_get/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="processamento.php" method="GET">
+        <div>
+            <input type="text" name="nome" placeholder="Peeencha seu nome">
+        </div>
+        <div>
+            <input type="text" name="idade" placeholder="Peeencha sua idade">
+        </div>
+        <div>
+            <input type="submit" name="Enviar">
+        </div>
+    </form>
+</body>
+</html>
